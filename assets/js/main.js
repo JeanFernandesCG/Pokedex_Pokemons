7d8328ba904ad900bd2b842a60354ecdf0b786b3
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-30 01:07:00 -0400

    botão + css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
-function converPokeTypes(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
-
 function convertPokemonHtml(pokemon) {
-  return `<li class="pokemon">
-          <span class="number">#${pokemon.order}</span>
+  return `<li class="pokemon ${pokemon.type}">
+          <span class="number">#${pokemon.number}</span>
           <span class="name"> ${pokemon.name} </span>
           <div class="detail">
             <ol class="types">
-              ${converPokeTypes(pokemon.types).join("")}
+              ${pokemon.types
+                .map((type) => `<li class="type ${type}">${type}</li>`)
+                .join("")}
             </ol>
             <img
-              src="${pokemon.sprites.other.dream_world.front_default}"
+              src="${pokemon.photo}"
               alt="${pokemon.name}"
             />
           </div>
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,26 @@
 const pokeApi = {};
+function convertPokeApiDetailPokemon(pokemonsDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokemonsDetail.order;
+  pokemon.name = pokemonsDetail.name;
+
+  const types = pokemonsDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+  pokemon.photo = pokemonsDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
 
 pokeApi.getPokemonsDetails = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
   const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => response.json())
